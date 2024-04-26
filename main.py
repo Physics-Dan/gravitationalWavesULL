dba3176a92e0f699d3aa1239f9cef5e0331c6c09
TechDaniel <91759692+Physics-Dan@users.noreply.github.com>
2024-04-26 20:49:24 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,162 @@
+# Instalamos el modulo de ligo que contiene funciones que necesitaremos para interpretar los datos, disponible en https://www.gw-openscience.org/static/sample_code/readligo.py
+! pip install readligo
+import readligo as rl
+# Descargamos 4096 archivos de datos secundarios a la señal de GW150914
+! wget -q -N https://www.gw-openscience.org/GW150914data/L-L1_LOSC_4_V2-1126257414-4096.hdf5
+! wget -q -N https://www.gw-openscience.org/GW150914data/H-H1_LOSC_4_V2-1126257414-4096.hdf5
+    
+# Otros 32 
+! wget -q -N https://www.gw-openscience.org/GW150914data/H-H1_LOSC_4_V2-1126259446-32.hdf5
+! wget -q -N https://www.gw-openscience.org/GW150914data/L-L1_LOSC_4_V2-1126259446-32.hdf5
+    
+# Descargamos un modelo de señal para poder hacer una comparación una vez tengamos datos sin ruido
+! wget -q -N https://www.gw-openscience.org/GW150914data/P150914/fig2-unfiltered-waveform-H.txt
+    
+# Descargamos datos de la figura 1 del paper de detección de GW150914 
+! wget -q -N https://www.gw-openscience.org/GW150914data/P150914/fig1-observed-H.txt
+! wget -q -N https://www.gw-openscience.org/GW150914data/P150914/fig1-residual-H.txt
+! wget -q -N https://www.gw-openscience.org/GW150914data/P150914/fig1-observed-L.txt
+! wget -q -N https://www.gw-openscience.org/GW150914data/P150914/fig1-residual-L.txt
+
+# Importamos varias librerías de python
+import numpy as np
+import scipy
+from scipy import signal
+from scipy.interpolate import interp1d
+from scipy.signal import butter, filtfilt
+import h5py
+import json
+
+%matplotlib inline
+import matplotlib
+import matplotlib.pyplot as plt
+import matplotlib.mlab as mlab
+
+# Parámetros utiles a la hora de hacer plot de los datos
+plt.rcParams['figure.figsize'] = [5, 3]
+plt.rcParams['lines.linewidth'] = .75
+
+
+# ------- densidad de las líneas espectrales del ruido ------------------
+
+# these 4096 second files are useful for computing more accurate psds
+large_data_filenames = {'GW150914': 'LOSC_4_V2-1126257414-4096.hdf5'}
+fn_H1 = 'H-H1_' + large_data_filenames['GW150914']
+fn_L1 = 'L-L1_' + large_data_filenames['GW150914']
+
+# this is the approximate event time which is used in the papers
+time_center = 1126259462
+
+# get sample rate from the H1 data file
+with h5py.File(fn_H1, 'r') as hdf_file:
+    dt = hdf_file['strain/Strain'].attrs['Xspacing'] #obtiene el período de muestreo de la observación
+fs = int(1.0/dt)
+print("Using sample rate of {0} Hz found in file: {1}".format(fs, fn_H1))
+
+
+def get_full_psds(eventnames, large_data_filenames,
+                  make_plots=False, plot_others=False):
+    """Obtains full 1024 second psds for all the events specified. Uses the Welch
+    average technique, along with other less accurate techniques if
+    specified. Can also plot the psd obtained.
+    
+    Args: 
+        eventnames (list): list of events to get psds for
+        large_datafilenames (dict): dictionary whose keys are the eventnames 
+            and whose values are the filenames of the large amounts of strain
+            data used, without the added 'H-<det>_'
+        make_plots (bool, optional): if set to True, plot psd data
+        plot_others (bool, optional): if set to True, also obtain psd data 
+            without averaging as well as with no window
+    
+    Returns:
+        dict: A dictionary containing psds for each detector for each event 
+            specified in eventnames.
+    """
+
+    large_data_psds = {}
+    for eventname in eventnames:
+        large_data_psds[eventname] = {'H1': [], 'L1': []}
+
+        # get filename
+        fn_H1 = 'H-H1_' + large_data_filenames[eventname]
+        fn_L1 = 'L-L1_' + large_data_filenames[eventname]
+
+        # get strain data
+        strain_H1, time_H1, chan_dict_H1 = rl.loaddata(fn_H1, 'H1')
+        strain_L1, time_L1, chan_dict_L1 = rl.loaddata(fn_L1, 'L1')
+
+        # both H1 and L1 will have the same time vector, so:
+        time = time_H1
+
+        indxt_around = np.where((time >= time_center - 512) & (
+            time < time_center + 512))
+
+        # number of sample for the fast fourier transform:
+        NFFT = int( 4 * fs )           # Use 4 seconds of data for each fourier transform
+        NOVL = int( 1 * NFFT / 2 )     # The number of points of overlap between segments used in Welch averaging
+        psd_window = scipy.signal.tukey(NFFT, alpha=1./4)
+
+        Pxx_H1, freqs = mlab.psd(strain_H1[indxt_around], Fs=fs, NFFT=NFFT,
+                                 window=psd_window, noverlap=NOVL)
+        Pxx_L1, freqs= mlab.psd(strain_L1[indxt_around], Fs=fs, NFFT=NFFT, 
+                                window=psd_window, noverlap=NOVL)
+        
+
+        if (plot_others):
+            # smaller window if we're not doing Welch's method
+            short_indxt_away = np.where((time >= time_center - 2) & (
+                time < time_center + 2))
+            # psd using a tukey window but no welch averaging
+            tukey_Pxx_H1, tukey_freqs = mlab.psd(
+                strain_H1[short_indxt_away], Fs=fs, NFFT=NFFT, window=psd_window)
+            # psd with no window, no averaging
+            nowin_Pxx_H1, nowin_freqs = mlab.psd(
+                strain_H1[short_indxt_away], Fs=fs, NFFT=NFFT, 
+                window=mlab.window_none)
+
+        # We will use interpolations of the PSDs computed above for whitening:
+        psd_H1 = interp1d(freqs, Pxx_H1)
+        psd_L1 = interp1d(freqs, Pxx_L1)
+
+        large_data_psds[eventname]['H1'] = psd_H1
+        large_data_psds[eventname]['L1'] = psd_L1
+
+        if make_plots:
+            plt.figure(figsize=(8, 5))
+            # scale x and y axes
+            plt.xscale('log', base=2)
+            plt.yscale('log', base=10)
+
+            # plot nowindow, tukey, welch together 
+            plt.plot(nowin_freqs, nowin_Pxx_H1,'purple',label= 'No Window',
+                     alpha=.8, linewidth=.5)
+            plt.plot(tukey_freqs, tukey_Pxx_H1 ,'green',label='Tukey Window',
+                     alpha=.8, linewidth=.5)
+            plt.plot(freqs, Pxx_H1,'black',label='Welch Average', alpha=.8,
+                     linewidth=.5)
+
+            # plot 1/f^2
+            # give it the right starting scale to fit with the rest of the plots
+            # don't include zero frequency
+            inverse_square = np.array(list(map(lambda f: 1 / (f**2), 
+                                    nowin_freqs[1:])))
+            # inverse starts at 1 to take out 1/0
+            scale_index = 500 # chosen by eye to fit the plot
+            scale = nowin_Pxx_H1[scale_index]  / inverse_square[scale_index]
+            plt.plot(nowin_freqs[1:], inverse_square * scale,'red',
+                     label= r'$1 / f^2$', alpha=.8, linewidth=1)
+
+            plt.axis([20, 512, 1e-48, 1e-41])
+            plt.ylabel('Sn(t)')
+            plt.xlabel('Freq (Hz)')
+            plt.legend(loc='upper center')
+            plt.title('LIGO PSD data near ' + eventname + ' at H1')
+            plt.show()
+
+    return large_data_psds
+
+
+eventnames = ['GW150914']
+large_data_psds = get_full_psds(eventnames, large_data_filenames,
+                                make_plots=True, plot_others=True)
